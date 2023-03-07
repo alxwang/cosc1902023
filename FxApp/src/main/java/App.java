0fe93c754776de05772241e20e5834357bc81529
Alex Wang <wangxi@saskpolytech.ca>
2023-03-07 10:25:35 -0600

    Start JavaFx

---
+++ FxApp/src/main/java/App.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+public class App extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button btOK = new Button("OK");
+        Scene scene = new Scene(btOK, 200, 250);
+        stage.setTitle("MyJavaFX"); // Set the stage title
+        stage.setScene(scene); // Place the scene in the stage
+        stage.show(); // Display the stage
+    }
+}
